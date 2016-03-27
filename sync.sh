f98dc23791330abc7837ed5daf2cce67938581bc
Dieter Reuter <dieter.reuter@me.com>
2016-03-27 13:21:11 +0200

    Run sync.sh w/o user interaction

--- sync.sh
+++ sync.sh
@@ -22,14 +22,6 @@ function doIt() {
   ###bash ~/.dotfiles/osx-set-terminal-defaults.sh
 
 }
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/N) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
+doIt
 unset doIt
 source ~/.bash_profile
